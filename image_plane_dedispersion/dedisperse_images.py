ececf406748ef69661c50c519b415bbe42216923
Natasha Hurley-Walker <nhw@icrar.org>
2022-08-16 11:11:23 +0800

    Fixed passing dms to loop and S/N cut

--- image_plane_dedispersion/dedisperse_images.py
+++ image_plane_dedispersion/dedisperse_images.py
@@ -12,6 +12,7 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u
 from astropy.wcs import WCS
 import multiprocessing
+import copy
 
 DMP = True
 try:
@@ -180,6 +181,7 @@ def main(n, coords, dynspec, args):
 
     # Run a bunch of DM trials to create a DM curve
     dmcurve = DMCurve(dynspec)
+    print(args.dms)
     dmcurve.run_dmtrials(args.dms, freq_ref=args.freq_ref)
     dmcurve.calc_best_dm()
     DM = dmcurve.best_dm
@@ -189,7 +191,7 @@ def main(n, coords, dynspec, args):
     dynspec.dedisperse(DM, freq_ref=args.freq_ref)
     dynspec.fscrunch()
 
-    snr = peak / np.std(sc(dynspec.fscrunched))
+    snr = peak / sc(dynspec.fscrunched)
 
     if args.no_plots == False and snr > 6:
         # Plot the DM curve
@@ -257,7 +259,6 @@ if __name__ == "__main__":
 
 # Make a copy of the default DMs
     dms = args.dms
-    print(args.dms)
 
     # read in the images
     hdus = sorted(glob(f"{args.input}-t????-????-image.fits"))
@@ -270,34 +271,35 @@ if __name__ == "__main__":
     ymax = a[0].data.shape[3]
     w = WCS(a[0].header, naxis=2)
 
-    # TODO optimise this so you only read in |b| < 1 deg
-
     # make a (hyper-) cube
     arr = np.empty((tmax, cmax, ymax, xmax))
     print(arr.shape)
-    for t in range(0, tmax):
-        for c in range(0, cmax):
+    #for t in range(0, tmax):
+    #    for c in range(0, cmax):
+# Fast testing
+    for t in range(0, 3):
+        for c in range(0, 3):
             h = fits.open(f"{args.input}-t{t:04d}-{c:04d}-image.fits")
             arr[t, c, :, :] = h[0].data
 
-    
     # When using a pool, you need to send the arguments as a tuple through the
     # wrapping function, so we set up the arguments here before running the pool
     pargs = []
     # We need to order our arguments by an index, since the results will be returned
     # in whatever order the pooled tasks finish
     n = 0
-    for x in range(0,xmax):
-        for y in range(0,ymax):
+#    for x in range(0,xmax):
+#        for y in range(0,ymax):
 # Fast testing
-#    for x in range(499,500):
-#        for y in range(0,1000):
+    for x in range(499,500):
+        for y in range(0,1000):
             coords = w.pixel_to_world(y, x)
             if np.abs(coords.galactic.b.value) < 2.1:
-                if DMP:
+                if DMP is True:
                     DM, tau_sc = pygedm.dist_to_dm(coords.galactic.l.value, coords.galactic.b.value, 10000, method='ymw16')
-                    args.dms[1] = DM.value
-                pargs.append((n, [x,y], arr[:, :, x, y], args))
+                    xargs = copy.deepcopy(args)
+                    xargs.dms[1] = DM.value
+                pargs.append((n, [x,y], arr[:, :, x, y], xargs))
                 n+=1
 
     # make a multiprocessing pool
