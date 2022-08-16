3d027d75d47d6c56fd4dee5727935eb4d922f1e4
Jared Moseley <jared1099@hotmail.com>
2022-08-16 17:16:11 +0800

    Addressed latest issues

--- mwax_offline_correlator_test/1344081136/offlineCorrScript.sh
+++ mwax_offline_correlator_test/1344081136/offlineCorrScript.sh
@@ -8,8 +8,8 @@ NUMTILES=136
 
 startChan=$1
 endChan=$2
-infile=$3
-outfile=$4
+indir=$3
+outdir=$4
 
 
 INPUT_BUFFER_SIZE=$(echo "$NUMTILES * 256000" | bc)
@@ -36,37 +36,26 @@ done
 #once all started create set proccess for each
 for CHAN in $(seq $startChan 1 $endChan); do
 
-mwax_db2fits -k "2${CHAN}${CHAN}2" --destination-path=${4} --health-netiface=lo --health-ip=224.0.2.2 --health-port=8005 > db2fits${CHAN}.log 2>&1 &
+mwax_db2fits -k "2${CHAN}${CHAN}2" --destination-path=outdir -l 0 --health-netiface=lo --health-ip=224.0.2.2 --health-port=8005 > db2fits${CHAN}.log 2>&1 &
 mwax_db2correlate2db "1${CHAN}${CHAN}1" "2${CHAN}${CHAN}2" 224.0.2.2 8004 -a 5 -b 160 -d 0 -f 6400 -o 1280 -O 2 -r -v -v > db2c2db${CHAN}.log 2>&1 &
 
 done
 
-#sleep here maybe?
 
 
+s
 
-
-#for each Channel/lines
-
-cd ${infile}
-#for CHAN in $(seq $startChan 1 $endChan); do 
-# $(ls *_${CHAN}.sub | sed 's/\(.*\)/dada_diskdb -k 1'${CHAN}${CHAN}'1 -f \1 \&\&/g' | tr '\n' ' ' | sed 's/\&\s$//');
-#done
+cd ${indir}
 
 ls *_${CHAN}.sub | sort | sed 's/\(.*\)/srun dada_diskdb -k 1'${CHAN}${CHAN}'1 -f \1/g' > ch${CHAN}.sh
 source ch${CHAN}.sh > ${destinationPath}/dada_diskdb${CHAN}.log 2>&1 &
 
 
+#kill buffers once the fits file is present
 for CHAN in $(seq $startChan 1 $endChan); do
     while [ ! -f ${4}/*ch${CHAN}_000.fits ]; do sleep 1; done
     dada_db -d -k "1${CHAN}${CHAN}1"
     dada_db -d -k "2${CHAN}${CHAN}2"
 done 
 
-
-
-#wait till last fits file exits
-
-
-#end
 
