0bce0ffe1aaebe585ab64281011be8ed7db67b0e
Sam McSweeney <sammy.mcsweeney@gmail.com>
2022-08-18 14:06:05 +0800

    Changed the way arguments are handled in dedispersion scripts

--- dynspec/lineup.py
+++ dynspec/lineup.py
@@ -1,17 +1,30 @@
 import matplotlib.pyplot as plt
 import dedisperse_dynspec as dd
 
-# Incl. Parkes
-#dlo = dd.Dynspec('1342623792_dyn_dynamic_spectrum.csv', sample_time=0.5, freqlo=72.955, bw=0.64, time_offset=1342623792, transpose=True)
-#dhi = dd.Dynspec('1342623496_dyn_dynamic_spectrum.csv', sample_time=0.5, freqlo=200.955, bw=1.28, time_offset=1342623496, transpose=True)
-
 # Two MWA ones
-dlo = dd.Dynspec('1342096104_dyn_dynamic_spectrum.csv', sample_time=0.5, freqlo=200.475, bw=0.64, time_offset=1342096104, transpose=True)
-dhi = dd.Dynspec('PKS_1342096266_dynamic_spectrum.csv', sample_time=0.1, freqlo=1216.0, bw=0.5, time_offset=1342096266.11, transpose=False)
+yamllo = "1342623792.yaml"
+yamlhi = "1342623496.yaml"
+with open(yamllo, 'r') as yaml:
+    params = dd.parse_yaml(yaml)
+    dlo = dd.Dynspec(**params)
+with open(yamlhi, 'r') as yaml:
+    params = dd.parse_yaml(yaml)
+    dhi = dd.Dynspec(**params)
+
+dlo.set_freq_ref('low')
+dhi.set_freq_ref('centre')
+
+# Incl. Parkes
+#dlo = dd.Dynspec(input='1342096104_dyn_dynamic_spectrum.csv', sample_time=0.5, freqlo=200.475, bw=0.64, t0=1342096104, transpose=True)
+#dhi = dd.Dynspec(input='PKS_1342096266_dynamic_spectrum.csv', sample_time=0.1, freqlo=1216.0, bw=0.5, t0=1342096266.11, transpose=False)
 
 DM = 288
-dlo.dedisperse(DM, freq_ref='low')
-dhi.dedisperse(DM, freq_ref='centre')
+
+dlo.add_dm_padding(DM, fill_value=0.0)
+dhi.add_dm_padding(DM, fill_value=0.0)
+
+dlo.dedisperse(DM)
+dhi.dedisperse(DM)
 
 dlo.t = dlo.get_time_at_infinite_frequency()
 dhi.t = dhi.get_time_at_infinite_frequency()
