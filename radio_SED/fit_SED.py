a4d7551e7e5cf2d8ea13280a2548f2243ff09473
Dr Natasha Hurley-Walker <nhw@icrar.org>
2022-09-30 15:23:41 +0800

    Turn the data into a radio SED

---
+++ radio_SED/fit_SED.py
@@ -0,0 +1,223 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+from matplotlib.ticker import FormatStrFormatter
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.size": 14})
+
+cm = 1/2.54
+
+# TOPCSV='GLEAM-X J065228.65-255000.34.csv'
+#TOPCSV = 'three_point_spectrum.csv'
+
+markers = {
+    'MWA':{'marker':'.', 'color':'black', 'label':'GLEAM-X', 'markeredgewidth':0.1, 'elinewidth':0.5},
+    'MeerKAT':{'marker':'s', 'color':'green', 'label':'MeerKAT', 'markersize':2},
+    'ASKAP':{'marker':'x', 'color':'blue', 'label':'ASKAP', 'markersize':2},
+    'Parkes':{'marker':'+', 'color':'red', 'label':'Parkes'}
+}
+
+ref_nu = 150
+
+def pl(nu, norm, alpha):
+    return norm * nu **alpha 
+
+def curved_law(nu, s_nu, alpha, q):
+    spec_nu = nu / ref_nu
+        
+    return s_nu * spec_nu ** alpha * \
+            np.exp(q * np.log(spec_nu)**2)
+
+def make_ax1(ax1, nu, df):
+    
+    mwa_mask = df.freq < 4000
+
+    ax1.errorbar(
+        df.freq[mwa_mask],
+        df.flux[mwa_mask],
+        yerr=df.fluxerr[mwa_mask],
+        linestyle='None',
+        **markers['MWA']
+    )
+# MeerKAT
+    corr = 0.09
+    df3 = pd.read_csv("radio_SED3.csv")
+    df3['flux'] /= corr
+    df3['fluxerr'] /= corr
+    ax1.errorbar(
+        df3.freq,
+        df3.flux,
+        yerr=df3.fluxerr,
+        linestyle='None',
+        **markers['MeerKAT']
+    )
+# MeerKAT
+
+
+# Good initials for two_comp_model
+#    fit_p0 = [np.median(df.flux), -0.7, 1.5, 2]
+# Initialisation for cureved_law
+    fit_p0 = [np.median(df.flux), -0.7, 0.0]
+    fit_res = curve_fit(
+        curved_law,
+        df.freq,
+        df.flux,
+        fit_p0,
+        sigma=df.fluxerr,
+        absolute_sigma=True
+    )
+
+    no_samps = 1000
+    samps = np.random.multivariate_normal(
+        fit_res[0], fit_res[1], size=no_samps
+    ).swapaxes(0,1)
+    
+    #freq = nu
+    models = curved_law(
+        nu[:, None],
+        *samps
+    )
+
+    q16, q50, q84 = np.percentile(models, [16, 50, 84], axis=1)
+    alpha = fit_res[0][1]
+    q = fit_res[0][2]
+    
+    ax1.plot(
+        nu,
+        q50,
+        lw=0.5,
+    )
+    ax1.fill_between(
+        nu,
+        q16, q84, alpha=0.3
+    )
+
+    model = (pl, (np.median(df.flux), -0.7), 'Power Law')
+
+    nu = np.geomspace(70, 3000, 100)
+    fit_func = model[0]
+    fit_p0 = model[1]
+    fit_res = curve_fit(
+        fit_func,
+        df.freq,
+        df.flux,
+        fit_p0,
+        sigma=df.fluxerr,
+        absolute_sigma=True
+    )
+    print(fit_res[0])
+
+    no_samps = 1000
+    samps = np.random.multivariate_normal(
+        fit_res[0], fit_res[1], size=no_samps
+    ).swapaxes(0,1)
+    
+    freq = nu
+    models = fit_func(
+        nu[:, None],
+        *samps
+    )
+
+    q16, q50, q84 = np.percentile(models, [16, 50, 84], axis=1)
+    
+    ax1.plot(
+        nu,
+            q50,
+            lw=0.5,
+        )
+    ax1.fill_between(
+        nu,
+        q16, q84, alpha=0.3
+        )
+
+    ax1.set(
+        xscale='log',
+        yscale='log',
+        xlabel='Frequency (MHz)',
+        ylabel='Flux density (Jy)',
+    )
+    ax1.xaxis.set_major_formatter(FormatStrFormatter('%3.2f'))
+    ax1.yaxis.set_major_formatter(FormatStrFormatter('%3.2f'))
+    return alpha, q
+
+def make_sed_figure(df, output='radio_SED.pdf'):
+
+    example_nu_large = np.geomspace(70, 3000, 100)
+
+    ax1_loc = (0.1, 0.1, 0.8, 0.8)
+
+    fig = plt.figure(figsize=(8.5, 6))
+
+    ax1 = fig.add_axes(ax1_loc)
+    alpha, q = make_ax1(ax1, example_nu_large, df)
+
+    fig.savefig(output, bbox_inches="tight")
+
+    return alpha, q
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Make some nice SED figure')
+
+    parser.add_argument('-o','--output', type=str, default='radio_SED.pdf', help='Name of the output file')
+
+    args = parser.parse_args()
+
+# MWA - MWA
+    df1 = pd.read_csv("radio_SED1.csv")
+# MWA - Parkes
+    df2 = pd.read_csv("radio_SED2.csv")
+# Scale df2 to match df1
+    mg = pd.merge(df1, df2, how='inner', on=['freq'])
+    corr = np.average(mg['flux_x']/mg['flux_y'])
+    df1['flux'] /= corr
+    df1['fluxerr'] /= corr
+
+# ASKAP
+    corr = 0.95
+    df4 = pd.read_csv("radio_SED4.csv")
+    df4['flux'] /= corr
+    df4['fluxerr'] /= corr
+
+    df = pd.concat([df1, df2, df4])
+
+    print(df)
+    
+    alpha, q = make_sed_figure(df,
+        output=args.output
+    )
+
+
+    print(alpha, q)
+    # From Wolfram Alpha
+    # integrate (Power[\(40)Divide[x,Power[10,9]]\(41),-1.08])*exp(-0.60*ln(Power[\(40)Divide[x,Power[10,9]]\(41),2])) over x = Power[10,7] to Power[10,14]
+
+    intr = 3.06e11
+
+    # From YMW 2017 calculator
+    # http://119.78.162.254/dmodel/index.php?mode=Gal&gl=22.15264754&gb=-2.06298163&dm=275&DM_Host=&ndir=1
+    d = 5.8
+    kpc = 3.086e+19
+    Jy2Wm = 1.e-26
+    Wm2ergs = 1.e7
+
+    # Very small opening angle
+    rho = 0.2
+    print("Radio luminosity {0:2.2e} erg/s for rho={1}deg".format(intr * Jy2Wm * Wm2ergs *(2*np.pi*((d * kpc)**2)/(0.06))*(1-np.cos(np.radians(rho))),rho))
+
+    # More reasonable, maybe?
+    rho = 2
+    print("Radio luminosity {0:2.2e} erg/s for rho={1}deg".format(intr * Jy2Wm * Wm2ergs *(2*np.pi*((d * kpc)**2)/(0.06))*(1-np.cos(np.radians(rho))),rho))
+
+    # Spin-down luminosity
+    P = 1318.19578
+    Pdot = 1.e-13
+
+    print("Spin-down luminosity < {0:2.2e} erg/s".format((4*np.pi*np.pi*1.e45*Pdot)/(P**3)))
+
+
--- radio_SED/process_MWA_Parkes_pulse.py
+++ radio_SED/process_MWA_Parkes_pulse.py
@@ -32,18 +32,6 @@ with open("../dynspec/1342096266.yaml", "r") as parkes_yaml:
 dlo = dd.Dynspec(**mwa_params)
 dhi = dd.Dynspec(**parkes_params)
 
-#NB: none of these plots line up properly
-fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True)
-dhi.plot(ax)
-plt.xlim([dhi.t[0], dhi.t[-1]])
-plt.savefig("test_Parkes.png")
-
-fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
-dlo.plot(ax[1])
-dhi.plot(ax[0])
-plt.xlim([dhi.t[0], dlo.t[-1]])
-plt.savefig('pre_ddsp_lineup.png')
-
 # [SM:] I'll need to explain/document the purpose of freq_ref better... It has nothing to do with the absolute timing of the pulses
 # What was happening was that because a "weird" reference frequency was being used for dedispersion, the pulse was wrapped around to the other side of the window -- which DOES obviously affect the absolute timing of the pulse
 dlo.set_freq_ref(mwa_params['freq_ref'])
@@ -60,12 +48,7 @@ dhi.dedisperse(DM)
 dlo.t = dlo.get_time_at_infinite_frequency()
 dhi.t = dhi.get_time_at_infinite_frequency()
 
-#NB: none of these plots line up properly
-fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True)
-dhi.plot(ax)
-plt.xlim([dhi.t[0], dhi.t[-1]])
-plt.savefig("test_Parkes_ddsp.png")
-
+# Helper plot to check the alignment
 fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
 fig.suptitle("DM = {:.2f}".format(DM))
 dlo.plot(ax[1])
@@ -73,9 +56,25 @@ dhi.plot(ax[0])
 plt.xlim([dhi.t[0], dlo.t[-1]])
 plt.savefig('lineup.png')
 
+# Helper plot to improve flagging
+fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True)
+dhi.plot(ax)
+# Manually plot the future cut-off regions
+ax.axhline(1345, color='k', lw=0.5)
+ax.axhline(1520, color='k', lw=0.5)
+ax.axhline(1630, color='k', lw=0.5)
+ax.axhline(1680, color='k', lw=0.5)
+ax.axhline(1900, color='k', lw=0.5)
+ax.axhline(2120, color='k', lw=0.5)
+ax.axvline(1342096293.0, color='k')
+ax.axvline(1342096306.0, color='k')
+plt.xlim([dhi.t[0], dhi.t[-1]])
+plt.savefig("test_Parkes_ddsp.png")
+
 # Transpose
 dat_lo = dlo.dynspec.T
-dat_hi = dhi.dynspec[:,0:592].T
+#dat_hi = dhi.dynspec[:,0:592].T
+dat_hi = dhi.dynspec.T
 
 # Likewise for the frequency channels
 freq_lo = dlo.f
@@ -92,10 +91,10 @@ freq_hi = dhi.f
 
 profile_lo = np.nanmean(dat_lo, axis=1)
 # define RMS by a bit of the observation that has no pulse in it
-rms_lo = np.nanstd(profile_lo[0:250])
-# Define the time range manually so that we integrate over the same amount of the pulse
-tstart = 350
-tend = 520
+rms_lo = np.nanstd(profile_lo[75:100])
+# Define the time range based on common start of pulse manually so that we integrate over the same amount of the pulse
+tstart = np.intersect1d(np.where(dlo.t>1342096293.0),np.where(dlo.t<1342096294.0))[0]
+tend = np.intersect1d(np.where(dlo.t>1342096305.0),np.where(dlo.t<1342096306.0))[-1]
 mask_1D_lo = np.arange(tstart, tend)
 r = np.arange(0,len(profile_lo))
 
@@ -109,9 +108,20 @@ ax.plot(r, profile_lo)
 ax.plot(r[mask_1D_lo],profile_lo[mask_1D_lo], color="green")
 fig.savefig("profile_lo.png", bbox_inches="tight")
 
+# Export the sum of the data over various frequency bands
+fluxes = []
+# For the MWA data, four 7.68-MHz chunks
+fluxes.append(np.nansum(dat_lo[tstart:tend,0:12])/((tend - tstart) * 12))
+fluxes.append(np.nansum(dat_lo[tstart:tend,12:24])/((tend - tstart) * 12))
+fluxes.append(np.nansum(dat_lo[tstart:tend,24:36])/((tend - tstart) * 12))
+fluxes.append(np.nansum(dat_lo[tstart:tend,36:48])/((tend - tstart) * 12))
+freqs = [dlo.f[6], dlo.f[18], dlo.f[30], dlo.f[42]]
+
 profile_hi = np.nanmean(dat_hi, axis=1)
 rms_hi = np.nanstd(profile_hi[0:250])
-# Define the time range manually so that we integrate over the same amount of the pulse
+# Define the time range based on common start of pulse manually so that we integrate over the same amount of the pulse
+tstart = np.intersect1d(np.where(dhi.t>1342096293.0),np.where(dhi.t<1342096294.0))[0]
+tend = np.intersect1d(np.where(dhi.t>1342096305.0),np.where(dhi.t<1342096306.0))[-1]
 mask_1D_hi = np.arange(tstart, tend)
 r = np.arange(0,len(profile_hi))
 
@@ -126,26 +136,49 @@ ax.plot(r[mask_1D_hi], profile_hi[mask_1D_hi], color="green")
 fig.savefig("profile_hi.png", bbox_inches="tight")
 
 # Export the sum of the data over various frequency bands
-# For low-frequency data, two 10-MHz chunks
-#total_lo = np.sum(profile_lo[mask_1D_lo])
-#total_hi = np.sum(profile_hi[mask_1D_hi])
-#fluxes = []
-#fluxes.append(np.nansum(dat_lo[tstart:tend,0:125])/((tend - tstart) * 125))
-#fluxes.append(np.nansum(dat_lo[tstart:tend,125:250])/((tend - tstart) * 125))
-#
-## For the high-frequency data, four 7.68-MHz chunks
-#fluxes.append(np.nansum(dat_hi[tstart:tend,0:12])/((tend - tstart) * 12))
-#fluxes.append(np.nansum(dat_hi[tstart:tend,12:24])/((tend - tstart) * 12))
-#fluxes.append(np.nansum(dat_hi[tstart:tend,24:36])/((tend - tstart) * 12))
-#fluxes.append(np.nansum(dat_hi[tstart:tend,36:48])/((tend - tstart) * 12))
-#fluxes = np.array(fluxes)
-#
-#freqs = np.array([dlo.f[62], dlo.f[187], dhi.f[6], dhi.f[18], dhi.f[30], dhi.f[42]])
-#
-## For now, 10% errors
-#fluxerrs = 0.1*fluxes
-#
-#ind = np.arange(0,6)
-#
-#np.savetxt("radio_SED2.csv", np.vstack([ind, freqs, fluxes, fluxerrs]).T, delimiter=",", header="ind,freq,flux,fluxerr", fmt=("%0.0d","%3.3f","%5.3f","%5.3f"))
+# For Parkes data, three reliable pieces ignoring the bottom end where the baseline drift is very strong
+total_lo = np.sum(profile_lo[mask_1D_lo])
+total_hi = np.sum(profile_hi[mask_1D_hi])
+#ax.axhline(1345, color='k', lw=0.5)
+#ax.axhline(1520, color='k', lw=0.5)
+#ax.axhline(1630, color='k', lw=0.5)
+#ax.axhline(1680, color='k', lw=0.5)
+#ax.axhline(1900, color='k', lw=0.5)
+#ax.axhline(2120, color='k', lw=0.5)
+#ax.axvline(1342096293.0, color='k')
+# Helper plot to show profiles for each chunk
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel("timestep")
+ax.set_ylabel("flux density (Jy/beam)")
+ind0 = np.where(dhi.f==1345.0)[0][0]
+ind1 = np.where(dhi.f==1520.0)[0][0]
+profile_hi = np.nanmean(dat_hi[:,ind0:ind1], axis=1)
+ax.plot(r, profile_hi, label=f"{(1345+1520)/2:4.0f}MHz", lw=0.5)
+fluxes.append(np.nansum(dat_hi[tstart:tend,ind0:ind1])/((tend - tstart) * ind1-ind0))
+freqs.append(dhi.f[int((ind1+ind0)/2)])
+ind0 = np.where(dhi.f==1630.0)[0][0]
+ind1 = np.where(dhi.f==1680.0)[0][0]
+profile_hi = np.nanmean(dat_hi[:,ind0:ind1], axis=1)
+ax.plot(r, profile_hi, label=f"{(1630+1680)/2:4.0f}MHz", lw=0.5)
+fluxes.append(np.nansum(dat_hi[tstart:tend,ind0:ind1])/((tend - tstart) * ind1-ind0))
+freqs.append(dhi.f[int((ind1+ind0)/2)])
+ind0 = np.where(dhi.f==1900.0)[0][0]
+ind1 = np.where(dhi.f==2120.0)[0][0]
+profile_hi = np.nanmean(dat_hi[:,ind0:ind1], axis=1)
+ax.plot(r, profile_hi, label=f"{(1900+2120)/2:4.0f}MHz", lw=0.5)
+fluxes.append(np.nansum(dat_hi[tstart:tend,ind0:ind1])/((tend - tstart) * ind1-ind0))
+freqs.append(dhi.f[int((ind1+ind0)/2)])
+ax.legend()
+fig.savefig("profile_comparison.pdf", bbox_inches="tight")
+fluxes = np.array(fluxes)
+freqs = np.array(freqs)
+
+
+# For now, 10% errors
+fluxerrs = 0.1*fluxes
+
+ind = np.arange(0,7)
+
+np.savetxt("radio_SED2.csv", np.vstack([ind, freqs, fluxes, fluxerrs]).T, delimiter=",", header="ind,freq,flux,fluxerr", fmt=("%0.0d","%3.3f","%5.5f","%5.5f"))
 
