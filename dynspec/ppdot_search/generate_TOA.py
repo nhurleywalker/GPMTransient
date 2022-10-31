811eeff869d689eb9be6ed3924b9c690443c2301
Sam McSweeney <sammy.mcsweeney@gmail.com>
2022-10-31 13:30:43 +0800

    A few extra lines in generate_TOA.py, WIP

--- dynspec/ppdot_search/generate_TOA.py
+++ dynspec/ppdot_search/generate_TOA.py
@@ -2,8 +2,26 @@ import numpy as np
 import yaml
 import argparse
 
-def main():
-    pass
+import sys
+from os import path
+sys.path.append("..")
+import dedisperse_dynspec as dd
+
+def main(args):
+    # Read in the metadata from the YAML file
+    metadata = dd.parse_yaml(args.yaml)
+    print(metadata)
+
+    # Generate a Gaussian with the specified width, but with the
+    #width_
 
 if __name__ == '__main__':
-    main()
+    # Parse the command line
+    parser = argparse.ArgumentParser(description='Generate TOAs from lightcurves using a basic low-pass Gaussian filter')
+    parser.add_argument('yaml', type=argparse.FileType('r'), help='The yaml file containing the meta information for the observation')
+    parser.add_argument('lightcurve', type=str, help='The two-column ASCII file containing the lightcurve data')
+    parser.add_argument('kernel_width_in_sec', type=float, help='The 1σ width of the Gaussian used for the filter (in seconds)')
+
+    args = parser.parse_args()
+
+    main(args)
