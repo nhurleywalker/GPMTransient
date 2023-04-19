fbb71d890efac67acf79c8cb8cbe1f3859d2568c
Natasha Hurley-Walker <nhw@icrar.org>
2023-04-19 20:13:37 +0800

    Added legend

--- pulse_flux_histogram/plot_histogram.py
+++ pulse_flux_histogram/plot_histogram.py
@@ -8,6 +8,8 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.font_manager
 from matplotlib import rc
+from matplotlib.patches import Rectangle
+
 import pandas as pd
 
 # Nature requires sans-serif fonts
@@ -23,12 +25,17 @@ df = pd.read_csv("pulse_table.csv")
 #https://www.nature.com/nature/for-authors/final-submission#:~:text=For%20guidance%2C%20Nature's%20standard%20figure,(120%E2%80%93136%20mm).
 fig = plt.figure(figsize=(8.9*cm,7.8*cm))
 ax1 = fig.add_subplot(111)
-ax1.hist(df["Peak flux density at 1 GHz (mJy)"][df["Fluence at 1 GHz (Jy s)"]>0], color='red', alpha=0.5)
+h1 = ax1.hist(df["Peak flux density at 1 GHz (mJy)"][df["Fluence at 1 GHz (Jy s)"]>0], color='red', alpha=0.5, label="$S$")
 ax2 = ax1.twiny()
-ax2.hist(df["Fluence at 1 GHz (Jy s)"][df["Fluence at 1 GHz (Jy s)"]>0], color='blue', alpha=0.5)
+h2 = ax2.hist(df["Fluence at 1 GHz (Jy s)"][df["Fluence at 1 GHz (Jy s)"]>0], color='blue', alpha=0.5, label="Fluence")
 ax2.set_xlabel("Fluence$_\mathrm{1GHz}$ (Jy s)")
 ax1.set_xlabel("$S_\mathrm{1GHz}$ (mJy)")
 ax1.set_ylabel("Number of pulses")
 ax1.set_yticks([0, 5, 10, 15, 20])
 ax1.yaxis.set_major_formatter(FormatStrFormatter('%3.0f'))
+# https://stackoverflow.com/questions/5484922/secondary-axis-with-twinx-how-to-add-to-legend
+colors = ['red', 'blue']
+handles = [Rectangle((0, 0), 1, 1, color=c, alpha=0.5) for c in colors]
+labels = ["Fluence", "$S$"]
+plt.legend(handles, labels)
 fig.savefig("S_hist.pdf", bbox_inches="tight", dpi=300)
