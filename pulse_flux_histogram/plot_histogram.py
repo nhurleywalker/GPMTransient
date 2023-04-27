e128f8e671a740e7fae9477901c9544dd51fbb61
Natasha Hurley-Walker <nhw@icrar.org>
2023-04-27 19:31:37 +0800

    More bins

--- pulse_flux_histogram/plot_histogram.py
+++ pulse_flux_histogram/plot_histogram.py
@@ -23,15 +23,16 @@ df = pd.read_csv("pulse_table.csv")
 # Pulse number,UTC,Barycentred TOA (MJD),Telescope,Frequency (MHz),Peak flux density at freq (Jy),Peak flux density at 1 GHz (mJy),Fluence at freq (Jy s),Fluence at 1 GHz (Jy s)
 
 #https://www.nature.com/nature/for-authors/final-submission#:~:text=For%20guidance%2C%20Nature's%20standard%20figure,(120%E2%80%93136%20mm).
+nbins = 22
 fig = plt.figure(figsize=(8.9*cm,7.8*cm))
 ax1 = fig.add_subplot(111)
-h1 = ax1.hist(df["Peak flux density at 1 GHz (mJy)"][df["Fluence at 1 GHz (Jy s)"]>0], color='red', alpha=0.5, label="$S$")
+h1 = ax1.hist(df["Peak flux density at 1 GHz (mJy)"][df["Fluence at 1 GHz (Jy s)"]>0], color='red', alpha=0.5, label="$S$", bins=nbins)
 ax2 = ax1.twiny()
-h2 = ax2.hist(df["Fluence at 1 GHz (Jy s)"][df["Fluence at 1 GHz (Jy s)"]>0], color='blue', alpha=0.5, label="Fluence")
+h2 = ax2.hist(df["Fluence at 1 GHz (Jy s)"][df["Fluence at 1 GHz (Jy s)"]>0], color='blue', alpha=0.5, label="Fluence", bins=nbins)
 ax2.set_xlabel("Fluence$_\mathrm{1GHz}$ (Jy s)")
-ax1.set_xlabel("$S_\mathrm{1GHz}$ (mJy)")
+ax1.set_xlabel("Peak $S_\mathrm{1GHz}$ (mJy)")
 ax1.set_ylabel("Number of pulses")
-ax1.set_yticks([0, 5, 10, 15, 20])
+#ax1.set_yticks([0, 5, 10, 15, 20])
 ax1.yaxis.set_major_formatter(FormatStrFormatter('%3.0f'))
 # https://stackoverflow.com/questions/5484922/secondary-axis-with-twinx-how-to-add-to-legend
 colors = ['red', 'blue']
